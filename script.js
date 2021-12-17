c9e9ef9c53bf47eb85ebd3117c2848f2b2213d20
Irinella Boutsika <3vilbonobo@gmail.com>
2021-12-18 00:31:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
-let text =
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam fugit ipsa? Explicabo itaque voluptas voluptatum repudiandae eveniet, dignissimos facilis expedita ipsa, asperiores sunt quisquam ad laboriosam nisi quae temporibus';
-let wordList = text.split(' ');
-let container = document.querySelector('.container');
+const text =
+  'My life fades, the vision dims. All that remains are memories. I remember a time of chaos, ruined dreams, this wasted land. But most of all, I remember the Road Warrior, the man we called Max. To understand who he was, you have to go back to another time when the world was powered by the black fuel and the deserts sprouted great cities of pipe and steel. Gone now, swept away. ';
+const wordList = text.split(' ');
+const container = document.querySelector('.container');
 let red, blue, green;
 
 //Random colors
-function colorChanger() {
+const colorChanger = () => {
   red = Math.floor(Math.random() * 255);
   blue = Math.floor(Math.random() * 255);
   green = Math.floor(Math.random() * 255);
-}
+};
 
 //Random element positioning
-function scatterWords() {
+const scatterWords = () => {
   leftValue = Math.floor(Math.random() * window.innerWidth - 100);
   topValue = Math.floor(Math.random() * window.innerHeight - 100);
-}
+};
 
 //Random blinking
-function randomBlink() {
+const randomBlink = () => {
   let min = 5;
   let max = 10;
   animDuration = Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 // Element creation
-for (let i = 0; i < wordList.length; i++) {
-  let word = document.createElement('p');
-  word.innerText = wordList[i];
-  colorChanger();
-  word.style.color = `rgb(${red}, ${blue}, ${green})`;
-  scatterWords();
-  word.style.top = `${topValue}px`;
-  word.style.left = `${leftValue}px`;
-  randomBlink();
-  word.style.animationDuration = `${animDuration}s`;
-  container.appendChild(word);
-}
+const creatingWords = () => {
+  container.innerHTML = '';
+  for (let i = 0; i < wordList.length; i++) {
+    let word = document.createElement('p');
+    word.innerText = wordList[i];
+    colorChanger();
+    word.style.color = `rgb(${red}, ${blue}, ${green})`;
+    scatterWords();
+    word.style.top = `${topValue}px`;
+    word.style.left = `${leftValue}px`;
+    randomBlink();
+    word.style.animationDuration = `${animDuration}s`;
+    container.appendChild(word);
+  }
+};
+creatingWords();
 
-// Collision detection
+//Detect window resizing
+window.addEventListener('resize', creatingWords);
